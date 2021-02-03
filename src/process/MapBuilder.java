c31950da6cf4c50262ac457e09e89e2e2d43f677
Polo95 <paulchevalier95@gmail.com>
2021-02-03 19:52:02 +0100

    modified MapBuilder constructor and MapRepository

--- src/process/MapBuilder.java
+++ src/process/MapBuilder.java
@@ -1,11 +1,65 @@
 package process;
 
 import model.Map;
+import model.POIType;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+import java.time.format.DateTimeFormatterBuilder;
+
 
 public class MapBuilder {
-
-    public MapBuilder(){
-
+    private MapRepository mapRepository = MapRepository.getInstance();
+    public MapBuilder(String path){
+        File file = new File(path);
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        try {
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.parse(file);
+            doc.getDocumentElement().normalize();
+            NodeList nl = doc.getElementsByTagName("node");
+            for (int indexNode=0; indexNode<nl.getLength(); indexNode++){
+                Node nd = nl.item(indexNode);
+                if(nd.getNodeType()== Node.ELEMENT_NODE){
+                    Element elt = (Element)nd;
+                    String posX = elt.getAttribute("x");
+                    String posY = elt.getAttribute("y");
+                    Element poi = (Element) elt.getElementsByTagName("poi").item(0);
+                   String type = null;
+                   String name = null;
+                    if(poi != null){
+                         type = poi.getAttribute("type");
+                        name = poi.getTextContent();
+                    }
+                    model.Node node = NodeFactory.creatNode(Float.parseFloat(posX),Float.parseFloat(posY), name, POIType.valueOf(type));
+                    mapRepository.addNode(node);
+                    NodeList nodeList = elt.getElementsByTagName("adjacentNode");
+                    for(int indexAdjacentNode=0; indexAdjacentNode<nodeList.getLength(); indexAdjacentNode++){
+                        Node ndAdjacent = nodeList.item(indexAdjacentNode);
+                        if(ndAdjacent.getNodeType()== Node.ELEMENT_NODE){
+                            Element eltAdjacent = (Element)ndAdjacent;
+                            String posNDAdjacentX = eltAdjacent.getAttribute("x");
+                            String posNDAdjacentY = eltAdjacent.getAttribute("y");
+                            String typeAdjacent = eltAdjacent.getAttribute("type");
+                        }
+                    }
+                }
+            }
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public Map buildMap() {
--- src/process/MapRepository.java
+++ src/process/MapRepository.java
@@ -1,14 +1,18 @@
 package process;
 
 import model.Node;
+import model.WayIdentifier;
 
 import java.util.HashMap;
 
 public class MapRepository {
     private HashMap<String, Node> nodes;
     private static MapRepository instance = new MapRepository();
+    private HashMap<String,HashMap<String, WayIdentifier>> ways;
 
     private MapRepository(){
+        nodes = new HashMap<>();
+        ways = new HashMap<>();
     }
 
     public static MapRepository getInstance() {
@@ -18,7 +22,10 @@ public class MapRepository {
     public void addNode(Node node){
         nodes.put(node.getId(), node);
     }
-
+    public void addWay(String idNodeA, String idNodeB, WayIdentifier way){
+        ways.putIfAbsent(idNodeA,new HashMap<>());
+        ways.get(idNodeA).put(idNodeB, way);
+    }
     public Node getNode(String id){
         return nodes.get(id);
     }
