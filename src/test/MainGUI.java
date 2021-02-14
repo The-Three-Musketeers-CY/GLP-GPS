8f6a96fd74061f26ef93e33881aece5c82fa48ff
Derrmos <william1226etudiantpro@gmail.com>
2021-02-14 23:11:41 +0100

    Adding ButtonsPanel at East of the GUI

--- src/test/MainGUI.java
+++ src/test/MainGUI.java
@@ -5,6 +5,7 @@ import process.builders.MapBuilder;
 import view.MapView;
 
 import javax.swing.*;
+import javax.swing.plaf.basic.BasicBorders;
 import java.awt.*;
 
 public class MainGUI extends JFrame {
@@ -13,11 +14,13 @@ public class MainGUI extends JFrame {
 
     private static final Dimension IDEAL_MAIN_DIMENSION = new Dimension(800, 400);
 
-    private static final Dimension IDEAL_MAPVIEW_DIMENSION = new Dimension(800, 300);
+    private static final Dimension IDEAL_MAPVIEW_DIMENSION = new Dimension(800, 400);
 
     private Map map;
 
     private MapView mapView ;
+
+    private JPanel buttonsPanel = new JPanel();
 
     public MainGUI(String title, String mapPath) {
         super(title);
@@ -32,8 +35,17 @@ public class MainGUI extends JFrame {
     private void init() {
         Container contentPane = getContentPane();
         contentPane.setLayout(new BorderLayout());
+
+        buttonsPanel.setLayout(new GridLayout(5, 1));
+        buttonsPanel.setPreferredSize(new Dimension(200, 400));
+        // TODO : Set borders with buttonsPanel.setBorder();
+        buttonsPanel.add(new JLabel("test"));
+        buttonsPanel.add(new JButton("test"));
+
+        contentPane.add(BorderLayout.EAST, buttonsPanel);
+
         mapView.setPreferredSize(IDEAL_MAPVIEW_DIMENSION);
-        contentPane.add(mapView);
+        contentPane.add(BorderLayout.WEST, mapView);
 
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         pack();
