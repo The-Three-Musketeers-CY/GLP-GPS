f38c273d38a92483d82ee280325c0f151060e810
benwa <benjamin.walleth@gmail.com>
2021-02-14 19:43:07 +0100

    Creating Map display

--- src/test/MainGUI.java
+++ src/test/MainGUI.java
@@ -2,24 +2,29 @@ package test;
 
 import model.Map;
 import process.builders.MapBuilder;
+import view.MapView;
 
 import javax.swing.*;
 import java.awt.*;
 
-public class MapGUI extends JFrame {
+public class MainGUI extends JFrame {
 
     private static final long serialVersionUID = 1L;
 
     private static final Dimension IDEAL_MAIN_DIMENSION = new Dimension(800, 400);
 
-    private MapBuilder mapBuilder;
+    private static final Dimension IDEAL_MAPVIEW_DIMENSION = new Dimension(800, 300);
+
     private Map map;
 
-    public MapGUI(String title, String mapPath) {
+    private MapView mapView ;
+
+    public MainGUI(String title, String mapPath) {
         super(title);
 
-        mapBuilder = new MapBuilder(mapPath);
+        MapBuilder mapBuilder = new MapBuilder(mapPath);
         map = mapBuilder.buildMap();
+        mapView = new MapView(map);
 
         init();
     }
@@ -27,6 +32,8 @@ public class MapGUI extends JFrame {
     private void init() {
         Container contentPane = getContentPane();
         contentPane.setLayout(new BorderLayout());
+        mapView.setPreferredSize(IDEAL_MAPVIEW_DIMENSION);
+        contentPane.add(mapView);
 
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         pack();
@@ -34,10 +41,11 @@ public class MapGUI extends JFrame {
         setVisible(true);
         setPreferredSize(IDEAL_MAIN_DIMENSION);
         setResizable(false);
+        mapView.repaint();
     }
 
     public static void main(String[] args) {
-        new MapGUI("Test", "map.xml");
+        new MainGUI("Test", "map.xml");
     }
 
 }
---
+++ src/view/MapView.java
@@ -0,0 +1,105 @@
+package view;
+
+import model.Map;
+import model.Network;
+import model.Node;
+import process.builders.MapBuilder;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class MapView extends JPanel{
+
+    private Map map ;
+    private int zoom ;
+
+
+    private class Click implements MouseListener {
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            int x = e.getX();
+            int y = e.getY();
+
+            for(Node node : map.getNodes().values()){
+                if(node.getPosition().getX()*zoom <= x+10 && node.getPosition().getX()*zoom>= x-10
+                        && node.getPosition().getY()*zoom <= y+10 && node.getPosition().getY()*zoom >= y-10){
+                    if(node.isPOI())
+                    JOptionPane.showMessageDialog(MapView.this,node.getPoi().getName());
+                    else JOptionPane.showMessageDialog(MapView.this,"Ce n'est pas un POI");
+                }
+            }
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+
+        }
+    }
+
+    public MapView(Map map){
+        this.map = map ;
+        zoom = 5 ;
+        this.addMouseListener(new Click());
+    }
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D)g.create();
+
+        for (Network network : map.getNetworks().values()){
+           for(String nodeId1 : network.getWays().keySet()){
+               Node node1 = map.getNodes().get(nodeId1);
+               for(String nodeId2 : network.getWays().get(nodeId1).keySet()){
+                   Node node2 = map.getNodes().get(nodeId2);
+                   g2d.setStroke(new BasicStroke(15));
+                   g2d.drawLine((int)node1.getPosition().getX()*zoom,(int)node1.getPosition().getY()*zoom,(int)node2.getPosition().getX()*zoom,(int)node2.getPosition().getY()*zoom);
+               }
+           }
+        }
+        for(Node node : map.getNodes().values()){
+            g.setColor(nodeColorType(node));
+            g.fillOval((int)node.getPosition().getX()*zoom-5,(int)node.getPosition().getY()*zoom-5,10,10);
+        }
+    }
+    private Color nodeColorType(Node node){
+        Color color ;
+        switch (node.getPoi().getType()){
+            case ATTRACTION:{
+                color = Color.ORANGE ;
+                break;
+            }
+            case BUILDING:{
+                color = Color.BLUE;
+                break;
+            }
+            case STATION:{
+                color = Color.GRAY;
+                break;
+            }
+            default:
+                color = Color.BLACK;
+        }
+        return color;
+    }
+
+}
