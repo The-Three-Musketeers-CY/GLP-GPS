70843ebf88516e22042c5392c169cc53b32bea50
Derrmos <william1226etudiantpro@gmail.com>
2021-02-16 00:18:55 +0100

    Refactoring MainGUI & MapView to GLP

--- src/test/MainGUI.java
+++ src/test/MainGUI.java
@@ -1,11 +1,13 @@
 package test;
 
 import model.Map;
+import model.Node;
 import process.builders.MapBuilder;
 import view.MapView;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
 public class MainGUI extends JFrame {
 
@@ -14,9 +16,15 @@ public class MainGUI extends JFrame {
     private static final Dimension IDEAL_MAIN_DIMENSION = new Dimension(1000, 600);
     private static final Dimension IDEAL_MAPVIEW_DIMENSION = new Dimension(1000, 600);
 
-    private Map map;
+    private static final int MIN_DEC_POS_X = -500;
+    private static final int MIN_DEC_POS_Y = -500;
+    private static final int MAX_DEC_POS_X = 0;
+    private static final int MAX_DEC_POS_Y = 0;
 
+    private Map map;
     private MapView mapView ;
+
+    private JButton resetButton = new JButton("Reset default position");
 
     public MainGUI(String title, String mapPath) {
         super(title);
@@ -32,8 +40,18 @@ public class MainGUI extends JFrame {
         Container contentPane = getContentPane();
         contentPane.setLayout(new BorderLayout());
 
+        MapDraggedListener mapDraggedListener = new MapDraggedListener();
+        mapView.addMouseListener(mapDraggedListener);
+        mapView.addMouseMotionListener(mapDraggedListener);
+        POIClickListener poiClickListener = new POIClickListener();
+        mapView.addMouseListener(poiClickListener);
+
         mapView.setPreferredSize(IDEAL_MAPVIEW_DIMENSION);
         contentPane.add(BorderLayout.WEST, mapView);
+
+        ResetDefaultPosButtonListener resetDefaultPosButtonListener = new ResetDefaultPosButtonListener();
+        resetButton.addActionListener(resetDefaultPosButtonListener);
+        contentPane.add(BorderLayout.SOUTH, resetButton);
 
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         pack();
@@ -42,6 +60,108 @@ public class MainGUI extends JFrame {
         setPreferredSize(IDEAL_MAIN_DIMENSION);
         setResizable(false);
         mapView.repaint();
+    }
+
+    private class MapDraggedListener implements MouseMotionListener, MouseListener {
+
+        private int cursorPosX = 0;
+        private int cursorPosY = 0;
+
+        @Override
+        public void mouseDragged(MouseEvent e) {
+            setCursor(Cursor.getPredefinedCursor(Cursor.MOVE_CURSOR));
+            int dx = e.getX() - cursorPosX;
+            int dy = e.getY() - cursorPosY;
+
+            if (dx + mapView.getDecPosX() <= MAX_DEC_POS_X && dx + mapView.getDecPosX() >= MIN_DEC_POS_X) mapView.setNewDecX(dx + mapView.getDecPosX());
+            if (dy + mapView.getDecPosY() <= MAX_DEC_POS_Y && dy + mapView.getDecPosY() >= MIN_DEC_POS_Y) mapView.setNewDecY(dy + mapView.getDecPosY());
+            repaint();
+        }
+
+        @Override
+        public void mouseMoved(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+            cursorPosX = e.getX();
+            cursorPosY = e.getY();
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+            setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+            mapView.setDecPosX(mapView.getNewDecX());
+            mapView.setDecPosY(mapView.getNewDecY());
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+
+        }
+    }
+
+    private class POIClickListener implements MouseListener {
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            int x = e.getX();
+            int y = e.getY();
+
+            for(Node node : map.getNodes().values()){
+                if(node.getPosition().getX() + mapView.getNewDecX() <= x+6 && node.getPosition().getX() + mapView.getNewDecX() >= x-6
+                        && node.getPosition().getY() + mapView.getNewDecY() <= y+6 && node.getPosition().getY() + mapView.getNewDecY() >= y-6){
+                    if(node.isPOI())
+                        JOptionPane.showMessageDialog(mapView, node.getPoi().getName() + " | Type : " + node.getPoi().getType().toString(), "POI Info", JOptionPane.PLAIN_MESSAGE);
+                    else JOptionPane.showMessageDialog(mapView,"Ce n'est pas un POI", "PAS POI", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+
+        }
+
+    }
+
+    private class ResetDefaultPosButtonListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            mapView.setDecPosX(0);
+            mapView.setDecPosY(0);
+            mapView.setNewDecX(0);
+            mapView.setNewDecY(0);
+            mapView.repaint();
+        }
+
     }
 
     public static void main(String[] args) {
--- src/view/MapView.java
+++ src/view/MapView.java
@@ -10,11 +10,6 @@ import java.awt.event.*;
 
 public class MapView extends JPanel{
 
-    private static final int MIN_DEC_POS_X = -500;
-    private static final int MIN_DEC_POS_Y = -500;
-    private static final int MAX_DEC_POS_X = 0;
-    private static final int MAX_DEC_POS_Y = 0;
-
     private Map map ;
 
     private int newDecX = 0;
@@ -23,121 +18,8 @@ public class MapView extends JPanel{
     private int decPosX = 0;
     private int decPosY = 0;
 
-    private int cursorPosX = 0;
-    private int cursorPosY = 0;
-
-    private JButton resetButton = new JButton("Reset default position");
-
-    private class MapDraggedListener implements MouseMotionListener, MouseListener {
-
-        @Override
-        public void mouseDragged(MouseEvent e) {
-            setCursor(Cursor.getPredefinedCursor(Cursor.MOVE_CURSOR));
-            int dx = e.getX() - cursorPosX;
-            int dy = e.getY() - cursorPosY;
-
-            if (dx + decPosX <= MAX_DEC_POS_X && dx + decPosX >= MIN_DEC_POS_X) newDecX = dx + decPosX;
-            if (dy + decPosY <= MAX_DEC_POS_Y && dy + decPosY >= MIN_DEC_POS_Y) newDecY = dy + decPosY;
-            repaint();
-        }
-
-        @Override
-        public void mouseMoved(MouseEvent e) {
-
-        }
-
-        @Override
-        public void mouseClicked(MouseEvent e) {
-
-        }
-
-        @Override
-        public void mousePressed(MouseEvent e) {
-            cursorPosX = e.getX();
-            cursorPosY = e.getY();
-        }
-
-        @Override
-        public void mouseReleased(MouseEvent e) {
-            setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
-            decPosX = newDecX;
-            decPosY = newDecY;
-        }
-
-        @Override
-        public void mouseEntered(MouseEvent e) {
-
-        }
-
-        @Override
-        public void mouseExited(MouseEvent e) {
-
-        }
-    }
-
-    private class POIClickListener implements MouseListener {
-
-        @Override
-        public void mouseClicked(MouseEvent e) {
-            int x = e.getX();
-            int y = e.getY();
-
-            for(Node node : map.getNodes().values()){
-                if(node.getPosition().getX() + newDecX <= x+6 && node.getPosition().getX() + newDecX >= x-6
-                        && node.getPosition().getY() + newDecY <= y+6 && node.getPosition().getY() + newDecY >= y-6){
-                    if(node.isPOI())
-                    JOptionPane.showMessageDialog(MapView.this, node.getPoi().getName() + " | Type : " + node.getPoi().getType().toString(), "POI Info", JOptionPane.PLAIN_MESSAGE);
-                    else JOptionPane.showMessageDialog(MapView.this,"Ce n'est pas un POI", "PAS POI", JOptionPane.ERROR_MESSAGE);
-                }
-            }
-        }
-
-        @Override
-        public void mousePressed(MouseEvent e) {
-
-        }
-
-        @Override
-        public void mouseReleased(MouseEvent e) {
-
-        }
-
-        @Override
-        public void mouseEntered(MouseEvent e) {
-
-        }
-
-        @Override
-        public void mouseExited(MouseEvent e) {
-
-        }
-
-    }
-
-    private class ResetDefaultPosButtonListener implements ActionListener {
-
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            decPosX = 0;
-            decPosY = 0;
-            newDecX = 0;
-            newDecY = 0;
-            repaint();
-        }
-
-    }
-
     public MapView(Map map){
         this.map = map;
-
-        setLayout(new BorderLayout());
-
-        addMouseListener(new POIClickListener());
-        addMouseMotionListener(new MapDraggedListener());
-        addMouseListener(new MapDraggedListener());
-
-        resetButton.addActionListener(new ResetDefaultPosButtonListener());
-        add(BorderLayout.SOUTH, resetButton);
     }
 
     @Override
@@ -182,6 +64,38 @@ public class MapView extends JPanel{
             }
         }
         return color;
+    }
+
+    public int getDecPosX() {
+        return decPosX;
+    }
+
+    public int getDecPosY() {
+        return decPosY;
+    }
+
+    public int getNewDecX() {
+        return newDecX;
+    }
+
+    public int getNewDecY() {
+        return newDecY;
+    }
+
+    public void setDecPosX(int decPosX) {
+        this.decPosX = decPosX;
+    }
+
+    public void setDecPosY(int decPosY) {
+        this.decPosY = decPosY;
+    }
+
+    public void setNewDecX(int newDecX) {
+        this.newDecX = newDecX;
+    }
+
+    public void setNewDecY(int newDecY) {
+        this.newDecY = newDecY;
     }
 
 }
