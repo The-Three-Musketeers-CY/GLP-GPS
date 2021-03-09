96b13a6f439dc87b2f30924e0a894cbc2dd7653e
benwa <benjamin.walleth@gmail.com>
2021-03-09 19:54:36 +0100

    Adding itinerary choice

--- src/config/GPSConfig.java
+++ src/config/GPSConfig.java
@@ -5,6 +5,12 @@ public class GPSConfig {
     public static final int WINDOW_WIDTH = 1000;
     public static final int WINDOW_HEIGHT = 600;
 
+    public static final int MAPVIEW_WIDTH = 750;
+    public static final int MAPVIEW_HEIGHT = 600;
+
+    public static final int ITINERARY_PANEL_WIDTH = 250 ;
+    public static final int ITINERARY_PANEL_HEIGHT = 200 ;
+
     public static final int MAP_SIZE_WIDTH = 1500;
     public static final int MAP_SIZE_HEIGHT = 1000;
 
