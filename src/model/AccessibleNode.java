681b11a79d36d5bf865166182bc96ae2123840a8
Polo95 <paulchevalier95@gmail.com>
2021-03-24 12:28:16 +0100

    Comment for the javadoc

--- src/model/AccessibleNode.java
+++ src/model/AccessibleNode.java
@@ -1,29 +1,54 @@
 package model;
 
+/**
+ * This class represents nodes that are accessible by other nodes
+ */
 public class AccessibleNode {
 
     private Node node ;
     private Node previousNode ;
     private float weight ;
 
+    /**
+     * This method construct the accessible node
+     * @param node node
+     * @param previousNode the previous node
+     * @param weight node weight
+     */
     public AccessibleNode(Node node, Node previousNode, float weight){
         this.node = node ;
         this.previousNode = previousNode ;
         this.weight = weight ;
     }
 
+    /**
+     * This method retrieves a node
+     * @return A node
+     */
     public Node getNode() {
         return node;
     }
 
+    /**
+     * This method retrieves a previous node
+     * @return A previous node
+     */
     public Node getPreviousNode() {
         return previousNode;
     }
 
+    /**
+     * This method retrieves the weight of a node
+     * @return The weight of the node
+     */
     public float getWeight() {
         return weight;
     }
 
+    /**
+     * This method sets the previous node
+     * @param previousNode the previous node
+     */
     public void setPreviousNode(Node previousNode) {
         this.previousNode = previousNode;
     }
--- src/model/identifiers/TransportIdentifier.java
+++ src/model/identifiers/TransportIdentifier.java
@@ -1,5 +1,8 @@
 package model.identifiers;
 
+/**
+ * This class lists the types of transport
+ */
 public enum TransportIdentifier {
 
     FOOT, CAR, BOAT, BUS, METRO, BICYCLE, PLANE, TRAIN
