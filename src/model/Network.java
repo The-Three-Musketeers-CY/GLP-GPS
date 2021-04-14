8a6a598fab0fa03311bbb472de19b90adbea01ed
Derrmos <denoyer.william@gmail.com>
2021-04-14 11:37:51 +0200

    Deleting Line class

--- src/model/Network.java
+++ src/model/Network.java
@@ -10,24 +10,12 @@ public class Network {
 
     private NetworkIdentifier type ;
     private HashMap<String,NodeWays> ways;
-    private HashMap<String,Line> lines;
     private WayIdentifier[] acceptedWays;
 
     public Network(NetworkIdentifier type, WayIdentifier[] acceptedWays){
         this.type = type ;
         ways = new HashMap<>();
-        lines = new HashMap<>();
         this.acceptedWays = acceptedWays;
-    }
-
-    public void addLine(String numLine){
-        Line line = new Line(numLine) ;
-        lines.put(numLine, line);
-    }
-
-    public void addStationToLine(String numLine,Node node){
-        Line line = lines.get(numLine) ;
-        line.addStation(node);
     }
 
     public void addWay(WayIdentifier identifier, Node node1, Node node2, String lineNumber){
@@ -48,10 +36,6 @@ public class Network {
         return ways.get(node.getId());
     }
 
-    public HashMap<String, Line> getLines() {
-        return lines;
-    }
-
     public boolean isAcceptedWay(WayIdentifier wayIdentifier) {
         for (WayIdentifier acceptedWay : acceptedWays) {
             if (acceptedWay == wayIdentifier) return true;
@@ -59,13 +43,11 @@ public class Network {
         return false;
     }
 
-
     @Override
     public String toString() {
         return "Network{" +
                 "type=" + type +
                 ", ways=" + ways +
-                ", lines=" + lines +
                 ", acceptedWays=" + Arrays.toString(acceptedWays) +
                 '}';
     }
