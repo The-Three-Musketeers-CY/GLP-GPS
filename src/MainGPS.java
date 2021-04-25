0a00ec5ddd9aba99cddeaab779c245cb4cdd0ace
Derrmos <denoyer.william@gmail.com>
2021-04-26 00:07:21 +0200

    Updating v4

--- src/MainGPS.java
+++ src/MainGPS.java
@@ -1,12 +1,10 @@
-package test;
-
 import config.GPSConfig;
 import gui.MainGUI;
 
 /**
  * This class is the main & test class of this program
  */
-public class TestGPS {
+public class MainGPS {
 
     public static void main(String[] args) {
         new MainGUI(GPSConfig.GPS_FRAME_NAME, GPSConfig.MAP_PATH);
--- src/config/GPSConfig.java
+++ src/config/GPSConfig.java
@@ -5,7 +5,7 @@ package config;
  */
 public class GPSConfig {
 
-    public static final String GPS_FRAME_NAME = "Test GPS - v4";
+    public static final String GPS_FRAME_NAME = "GPS - v4";
     public static final String MAP_PATH = "src/test/map/map.xml";
 
     public static final int WINDOW_WIDTH = 1000;
